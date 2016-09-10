6386181290c869c996c98b254ac9ac57c598eb32
Jake Jarrett <jakeryanjarrett@gmail.com>
2016-09-10 11:48:39 +1000

    Apparently the hot module replacement doesn't work. awesome
    Taking out the hmr code that was in there, results in the same reload anyway.
    
    Ok

--- src/modules/pages/features/views/features.js
+++ src/modules/pages/features/views/features.js
@@ -28,22 +28,6 @@ let FeaturesView = Marionette.View.extend({
     },
 
     /**
-     * Hot module replacement.
-     * Get them while they're hot!
-     */
-    initialize () {
-        if(module.hot){
-            module.hot.accept("modules/common/views/navigation/navigation", function(){
-                /** Re require sub views **/
-                let Nav = require("modules/common/views/navigation/navigation");
-                let Navigation =  new Nav.default();
-                App.getNavigationContainer().show(Navigation);
-                Navigation.setItemAsActive("home");
-            });
-        }
-    },
-
-    /**
      * On render, we want to add the navigation
      *
      * @protected
