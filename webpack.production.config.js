5dfb0cc194514edfd2b69018e3adc752bd533db6
Jake Jarrett <jake@vokke.com.au>
2016-09-23 15:39:39 +1000

    Fix production build & manifest url
    Manifest is now /manifest.json so it maintains lighthouse score of 80 on
    any url.
    
    Fixed production build (Would throw 404 about missing bundles before)

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,23 +1,34 @@
 var Webpack = require("webpack");
 var path = require("path");
+var srcPath = path.resolve(__dirname, "src");
 var nodeModulesPath = path.resolve(__dirname, "node_modules");
 var buildPath = path.resolve(__dirname, "public", "build");
-var mainPath = path.resolve(__dirname, "src", "main.js");
-var srcPath = path.resolve(__dirname, "src");
-
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 var autoprefixer = require("autoprefixer");
 var precss = require("precss");
 
 var config = {
-    // We change to normal source mapping, if you need them
+    context: __dirname,
     devtool: "source-map",
-    entry: mainPath,
+
+    entry: [
+        path.resolve(srcPath, "main.js")
+    ],
+
     output: {
         path: buildPath,
-        filename: "bundle.js"
+        filename: "[name].js",
+        publicPath: "/build/"
     },
+
     module: {
+        preLoaders: [
+            {
+                test: /\.json$/,
+                loader: "json"
+            }
+        ],
         loaders: [
             {
                 /** Compiles ES6 to ES5 **/
@@ -40,10 +51,10 @@ var config = {
                     fallbackLoader: "style?singleton",
                     loader: "!css-loader?locals&sourceMap!postcss!sass?sourceMap"
                 })
-
             }
         ]
     },
+
     plugins: [
         new Webpack.optimize.UglifyJsPlugin({minimize: true}),
         new ExtractTextPlugin("app.min.css")
