aeb7458ae1287ac461a2d505ac71326057a55598
Jake Jarrett <jake@vokke.com.au>
2016-10-24 10:38:22 +1100

    Remove babel-polyfill from webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ var config = {
     devtool: "source-map",
 
     entry: [
-        "babel-polyfill",
         "webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr",
         path.resolve(srcPath, "main.js")
     ],
